d8a508e3b298418d64f709b6ff6e3652d17cce21
lennonruthven <77502499+lennonruthven@users.noreply.github.com>
2021-02-04 02:50:59 +0000

    Add files via upload

---
+++ src/components/footer.tsx
@@ -0,0 +1,23 @@
+import ExtLink from './ext-link'
+
+export default () => (
+  <>
+    <footer>
+      <span></span>
+      <ExtLink>
+        <img
+          src=""
+          height={46}
+          width={132}
+          alt=""
+        />
+      </ExtLink>
+      <span>
+        {' '}
+        <ExtLink href="">
+        
+        </ExtLink>
+      </span>
+    </footer>
+  </>
+)
